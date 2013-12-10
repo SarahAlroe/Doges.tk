c4520283877d91e02f4efb803a323cff668d854e
SilasAlroe <silasalroe@gmail.com>
2013-12-11 00:47:34 +0100

    Built base of site

--- index.php
+++ index.php
@@ -1,2 +1,15 @@
-Much doge
-Very soon
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php
+		include $_SERVER['DOCUMENT_ROOT'] . '/lib/base.php';
+		?>
+	</head>
+	<body>
+		<div class="container">
+			<div class="headerimage">
+			<img style="width: 100%" src="/lib/img/Doges.Tk.png"></div>
+			<div class="contentbox"><img style="width: 100%" src="/lib/img/Doges.Tk.png"></div>
+		</div>
+	</body>
+</html>
---
+++ lib/base.php
@@ -0,0 +1,3 @@
+<link rel="stylesheet" type="text/css" href="http://doges.tk/lib/style.css" />
+<link rel="shortcut icon" href="/lib/img/favicon.ico">
+<title>DOGES</title>
