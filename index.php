39e82da82fde735b4103ae7392e1fb8b53af6118
SilasAlroe <silasalroe@gmail.com>
2014-03-21 00:20:47 +0100

    Added an autopopulate php script to the frontpage. Also added a few test doges.

--- index.php
+++ index.php
@@ -9,7 +9,24 @@
 		<div class="container">
 			<div class="headerimage">
 			<img style="width: 100%" src="/lib/img/Doges.Tk.png"></div>
-			<div class="contentbox"><img style="width: 100%" src="/lib/img/Doges.Tk.png"></div>
+			<div class="contentbox">
+				
+				<?php
+				$dogedir="lib/doges/";
+				foreach (glob($dogedir."*.{jpg,png,gif}", GLOB_BRACE) as $filename) {
+					echo "<div class='dogeoutcontainer'><div class='dogecontainer'>";
+					$explodefile=explode(".", $filename);
+					$basefile=$explodefile[0];
+					echo "<img class='dogeimg' alt='".$filename."' src='".$filename."'/>";
+					if(file_exists($basefile.".txt")){
+						echo "<br />";
+						include $basefile.".txt";
+						echo "<br />";
+					}
+					echo "</div></div><br />";
+				}
+				?>
+				</div>
 		</div>
 	</body>
 </html>
